ec0befd521d9381d858b0ad88602e6e00772b98c
gouthamkanukolluviswa <63440381+gouthamkanukolluviswa@users.noreply.github.com>
2021-05-29 22:56:24 +0530

    11th commit

---
+++ read_mat_abc.py
@@ -0,0 +1,29 @@
+#read 3 matrices a,b,c and test if ab=c
+from random import randint
+from sys import stdin
+def readint():
+    return int(stdin.readline())
+def readarray(typ):
+    return list(map(typ,stdin.readline().split()))
+def readmatrix(n):
+    M=[]
+    for _ in range(n):
+        row=readarray(int)
+        assert len(row)==n
+        M.append()
+    return M
+def mul(M,v):
+    n=len(M)
+    return [sum(M[i][j]*v[j] for j in range(n)) for i in range(n)]
+def freivalds(A,B,C):
+    n=len(A)
+    x=[randint(0,1000000) for j in range(n)]
+    return mul(A,mul(B,x))==mul(C,x)
+
+
+if __name__=="main":
+    n=readint()
+    A=readmatrix(n)
+    B=readmatrix(n)
+    C=readmatrix(n)
+
