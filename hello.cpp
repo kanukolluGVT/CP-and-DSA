8d63b6c6e87cee20494d6ad7d25da376abf55d22
Kanukollu Goutham Viswa Tej <63440381+kanukolluGVT@users.noreply.github.com>
2021-08-16 10:54:13 +0530

    heap-aditya verma-1

---
+++ hello.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i = 1; i <= n; i++)
+#define mod 1000000007
+#define pb push_back
+#define ff first
+#define ss second
+#define ii pair<int,int>6
+#define vi vector<int>
+#define vii vector<ii>
+#define lli long long int
+#define INF 1000000000
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+ 
+using namespace std;
+
+void dfile()
+{
+#ifndef ONLINE_JUDGE
+ 
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+ 
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+ 
+#endif
+}
+
+int main ()
+{
+
+    dfile();
+    
+    return 0;
+}
