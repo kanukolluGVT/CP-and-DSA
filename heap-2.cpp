0af3f5743ad7f57ccfc7a8533aef5d73b3dcdfba
Kanukollu Goutham Viswa Tej <63440381+kanukolluGVT@users.noreply.github.com>
2021-08-16 11:29:29 +0530

    Add files via upload

---
+++ heap-2.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i = 1; i <= n; i++)
+#define mod 1000000007
+#define pb push_back
+#define ff first
+#define ss second
+#define ii pair<int,int>6
+#define vi vector<int>
+#define vii vector<ii>
+#define lli long long int
+#define INF 1000000000
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+ 
+using namespace std;
+
+void dfile()
+{
+#ifndef ONLINE_JUDGE
+ 
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+ 
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+ 
+#endif
+}
+
+int main ()
+{
+
+    dfile();
+    int n,k;
+    cin>>n;
+    cin>>k;
+    int ar[n];
+    int ans[k];
+    for(int i=0;i<n;i++)
+        cin>>ar[i];
+    priority_queue<int> maxh;
+    for(int i=0;i<n;i++)
+    {
+        maxh.push(ar[i]);
+        if(maxh.size()>k)
+            maxh.pop();
+
+    }
+    for(int i=0;i<k;i++)
+    {
+        ans[i]=maxh.top();
+        maxh.pop();
+
+
+    }
+    //cout<<maxh.top();
+      
+    for(int i=0;i<k;i++){
+        cout<<ans[i]<<endls;
+
+    }
+
+
+
+
+
+
+
+    
+    return 0;
+    
+}
