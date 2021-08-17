1ba67451b398d2ae113b644cddce07f8bb98b74a
Kanukollu Goutham Viswa Tej <63440381+kanukolluGVT@users.noreply.github.com>
2021-08-17 11:15:29 +0530

    top k frequent elemenst aditya verma

---
+++ heap-5.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i = 1; i <= n; i++)
+#define mod 1000000007
+#define pb push_back
+#define ff first
+#define ss second
+#define ii pair<int,int>6
+#define vi vector<int>
+#define vii vector<ii>
+#define lli long long int
+#define INF 1000000000
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+ 
+using namespace std;
+
+void dfile()
+{
+#ifndef ONLINE_JUDGE
+ 
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+ 
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+ 
+#endif
+}
+
+int main ()
+{
+
+    dfile();
+    int n,k;
+    cin>>n;
+    cin>>k;
+    int ar[n];
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> p;
+    unordered_map<int,int> mp;
+    for(int i=0;i<n;i++){
+        cin>>ar[i];
+
+    }
+    for(int i=0;i<n;i++)
+    {
+        mp[ar[i]]++;
+    }
+    //for(auto i=mp.begin;i!=mp.end;i++)
+    for(auto i:mp)
+    {
+        //p.push({i->second,i->first});
+        p.push(make_pair(i.second,i.first));
+        if(p.size()>k)
+            p.pop();
+
+
+    }
+    //cout<<"hello"<<endl;
+    while(p.size()>0)
+    {
+        cout<<p.top().second<<endl;
+        p.pop();
+        
+        
+    }
+
+    return 0;
+}
